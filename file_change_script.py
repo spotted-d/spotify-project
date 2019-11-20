863b03cbbdf68d2095127e07b1cf398582ede5f5
Emily Xie <emilyxxie@gmail.com>
2019-11-19 22:06:03 -0500

    Write script to create unique pids

---
+++ file_change_script.py
@@ -0,0 +1,38 @@
+# Used stack overflow for a base: https://stackoverflow.com/questions/11070527/how-to-add-a-new-column-to-a-csv-file
+
+import csv
+# from os import listdir
+import os
+import sys
+import re
+# from os.path import isfile, join, getcwd
+
+directory_path = "Songs"
+
+working_directory = os.getcwd()
+
+all_files = [f"{working_directory}/{directory_path}/{f}" for f in os.listdir(directory_path)]
+
+# all_files = ["/Users/Emily/harvard_cse/spotify_data/test_directory/songs0.csv"]
+
+for file in all_files:
+
+    new_file = []
+    with open(file, "r") as csvinput:
+        reader = csv.reader(csvinput)
+        # This grabs the headers and re-creates a set of new ones
+        header = next(reader)
+        header.append("unique_pid")
+        file_num = re.search("songs(\d*).csv", file).groups()[0]
+        new_file.append(header)
+
+        for row in reader:
+            pid = row[0]
+            unique_pid = file_num + pid
+            row.append(unique_pid)
+            new_file.append(row)
+
+    with open(f"output_data/append_songs{file_num}.csv", 'w') as csvoutput:
+        print(f"Writing for file {file_num}")
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        writer.writerows(new_file)
